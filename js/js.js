438d7f73959587fe304405c6d373b1ac542e0552
Vicente <dev.vicentefigueroa@gmail.com>
2021-08-15 08:31:44 -0300

    domingo15

--- js/js.js
+++ js/js.js
@@ -1,1 +1,15 @@
 //Funcion de menu de hamburguesa
+
+function menu() {
+  var boton = document.getElementById("menu");
+  boton.style.display = "none";
+}
+
+function boton() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
